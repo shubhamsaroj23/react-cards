075486333c8467a75874c3e6e6195cc48f1e4322
shubham <shubham@shubhams-MacBook-Air.local>
2021-08-17 22:46:55 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Cars from "./component/Basics/Cars";
+
+const App = () => {
+  return <Cars />;
+};
+
+export default App;
---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,171 @@
+const Menu = [
+  {
+    id: 1,
+    image: "https://images.unsplash.com/photo-1532581140115-3e355d1ed1de?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bGFtYm9yZ2hpbml8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    name: "Lamborghini",
+    category: "Sport",
+    price: "12000000",
+    description:
+      "new lamo Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+
+  {
+    id: 2,
+    image: "https://images.unsplash.com/photo-1544829099-b9a0c07fad1a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fHNwb3J0cyUyMGNhcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    name: "Aventador",
+    category: "Sport",
+    price: "20000000",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 3,
+    image: "https://images.unsplash.com/photo-1562911791-c7a97b729ec5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1108&q=80",
+    name: "Chevy camaro",
+    category: "Sport",
+    price: "10000000",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 4,
+    image: "https://images.unsplash.com/photo-1582651201295-84d261733bb1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
+    name: "Ford Mustang",
+    category: "Sport",
+    price: "500909090",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 5,
+    image: "https://media.evo.co.uk/image/private/s--tIQxHuG3--/v1622811271/evo/2021/06/2021%20Rolls-Royce%20Ghost%20review%20-3.jpg",
+    name: "rolls royce",
+    category: "luxury",
+    price: "80k",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 6,
+    image: "https://images.unsplash.com/photo-1570828916393-27caeade170c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTJ8fGx1eHVyeSUyMGNhcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    name: "mercedes-benz",
+    category: "luxury",
+    price: "180₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 7,
+    image: "https://images.unsplash.com/photo-1563720223523-491ff04651de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    name: "range rover",
+    category: "luxury",
+    price: "60₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 8,
+    image: "https://images.unsplash.com/photo-1537984822441-cff330075342?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGJtdyUyMGNhcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    name: "BMW",
+    category: "luxury",
+    price: "60",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 9,
+    image: "https://images.unsplash.com/photo-1621695644555-5fe0f2f87939?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHN1dnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    name: "Jeep",
+    category: "SUV",
+    price: "10",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 10,
+    image: "https://images.unsplash.com/photo-1612563830298-e309dab9f129?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDF8fHN1dnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    name: "Kai",
+    category: "SUV",
+    price: "10",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+
+  {
+    id: 11,
+    image: "https://images.unsplash.com/photo-1560281340-6d9e200156f1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTR8fHN1dnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    name: "Audi",
+    category: "SUV",
+    price: "10",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+
+  {
+    id: 12,
+    image: "https://images.unsplash.com/photo-1527247043589-98e6ac08f56c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c3BvcnRzJTIwY2FyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    name: "ferrari",
+    category: "Sport",
+    price: "10",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+
+
+  {
+    id: 13,
+    image: "https://images.unsplash.com/photo-1605893477799-b99e3b8b93fe?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cGlja3VwJTIwdHJ1Y2t8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    name: "Ford",
+    category: "Pickup Truck",
+    price: "10",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 14,
+    image: "https://images.unsplash.com/photo-1608234493333-09825469ab3b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHBpY2t1cCUyMHRydWNrfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    name: "Toyota",
+    category: "Pickup Truck",
+    price: "10",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+
+  {
+    id: 15,
+    image: "https://images.unsplash.com/photo-1601252300554-4ad551483bd2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGlja3VwJTIwdHJ1Y2t8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    name: "chevrolet",
+    category: "Pickup Truck",
+    price: "10",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+
+
+  {
+    id: 16,
+    image: "https://images.unsplash.com/photo-1551727324-355cda9f1884?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGF1ZGl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    name: "Audi",
+    category: "Sport",
+    price: "12000000",
+    description:
+      "new lamo Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  }, 
+
+  {
+    id: 17,
+    image: "https://images.unsplash.com/photo-1596493688853-cc5fff2789cc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTF8fGF1ZGl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    name: "audi",
+    category: "luxury",
+    price: "80k",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+
+
+
+
+
+];
+
+export default Menu;
